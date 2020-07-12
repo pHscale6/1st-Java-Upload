e92bfdbfcf9acb381cbccdd55ed69afc42a59826
pHscale6 <68198458+pHscale6@users.noreply.github.com>
2020-07-12 11:41:52 -0600

    Add files via upload

---
+++ VarDataTypeAssignment.java
@@ -0,0 +1,22 @@
+// Paul Hyde
+public class VarDataTypeAssignment {
+	public static void main(String[] args) {
+		// Variable Declarations
+		String line = "===============================";
+		String UserName = "Jon ";
+		String username = "Johnson";
+		int customerAge = 22;
+		double gpa = 2.91;
+		String major = "Psychology";
+		String year = "Senior"; 
+		System.out.print(line);
+		System.out.println("\nSTUDENT REPORT");
+		System.out.print(line);
+		System.out.println("\nStudent Name:	" + UserName + username);
+		System.out.printf("Age: " + customerAge);
+		System.out.printf("         GPA: " + gpa);
+		System.out.println("\nMajor: " + major);
+		System.out.println("Class: " + year);
+		System.out.print(line);
+    }
+}
